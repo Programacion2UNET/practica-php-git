ebb230bccbfd64ebff4cfd8bef425a4b0a19931f
MariandreaMV <mariandrea.maldonado@labcomp.unet.edu.ve>
2018-02-22 09:10:41 -0430

    bienvenidos y salto de linea

--- index.php
+++ index.php
@@ -4,11 +4,14 @@
     </head>
     <body>
         <h1>Mi primer sistema CRUD</h1>
+        <h3>Bienvenidos</h3>
         <ul>
             <li>
 				<a href="views/create.php">Crear registro</a>
-				<a href="views/read.php">Leer registros</a>
 			</li>
+            <li>
+                <a href="views/read.php">Leer registros</a>
+            </li>
         </ul>
     </body>
 </html>
